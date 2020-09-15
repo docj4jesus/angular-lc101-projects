8e1105d8cd61f1a75a12cafec8241429562a41ec
Janell Renee Johnson <docj4jesus@gmail.com>
2020-09-15 16:19:50 -0500

    Lesson 1 Updates

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Coolest Photos of ALL Time';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'http://blog.timesunion.com/bark/files/2012/02/Dachshund.jpg';
+  image3 = 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pumpkins-1529604270.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=980:*';
 
   constructor() { }
 
